ddc8f74c3d7d6480b0ffced97157e05a9bd50711
SanskarKashyap <2005825@kiit.ac.in>
2024-04-23 22:09:50 +0530

    DNS

--- modules/dns.py
+++ modules/dns.py
@@ -10,69 +10,80 @@ C = '\033[36m'  # cyan
 W = '\033[0m'   # white
 Y = '\033[33m'  # yellow
 
-
 def dnsrec(domain, output, data):
-	result = {}
-	print(f'\n{Y}[!] Starting DNS Enumeration...{W}\n')
-	dns_records = ['A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'MX', 'NS', 'TXT']
-	full_ans = []
+    """
+    Perform DNS enumeration for a given domain.
 
-	for dns_record in dns_records:
-		query = dnslib.DNSRecord.question(domain, dns_record)
-		try:
-			pkt = query.send('8.8.8.8', 53, tcp='UDP')
-			ans = dnslib.DNSRecord.parse(pkt)
-			ans = str(ans)
-			ans = ans.split('\n')
-			full_ans.extend(ans)
-		except ConnectionRefusedError as exc:
-			print(f'\n{R}[-] {C}Exception : {W}{exc}\nServer is probably not listening!')
-			log_writer(f'[dns] Exception = {exc}')
-			return
+    Args:
+        domain (str): The domain to perform DNS enumeration on.
+        output (str): The output configuration for saving the results.
+        data (dict): The data dictionary to store the results.
+    """
+    result = {}
+    print(f'\n{Y}[!] Starting DNS Enumeration...{W}\n')
+    dns_records = ['A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'MX', 'NS', 'TXT']
+    full_ans = []
 
-	full_ans = set(full_ans)
-	dns_found = []
+    # Query DNS Records for various record types
+    for dns_record in dns_records:
+        query = dnslib.DNSRecord.question(domain, dns_record)
+        try:
+            pkt = query.send('8.8.8.8', 53, tcp='UDP')
+            ans = dnslib.DNSRecord.parse(pkt)
+            ans = str(ans)
+            ans = ans.split('\n')
+            full_ans.extend(ans)
+        except ConnectionRefusedError as exc:
+            print(f'\n{R}[-] {C}Exception : {W}{exc}\nServer is probably not listening!')
+            log_writer(f'[dns] Exception = {exc}')
+            return
 
-	for entry in full_ans:
-		if not entry.startswith(';'):
-			dns_found.append(entry)
+    full_ans = set(full_ans)
+    dns_found = []
 
-	if not dns_found:
-		print(f'{R}[-] {C}DNS Records Not Found!{W}')
-		if output != 'None':
-			result.setdefault('dns', ['DNS Records Not Found'])
-	else:
-		for entry in dns_found:
-			print(f'{C}{entry}{W}')
-			if output != 'None':
-				result.setdefault('dns', []).append(entry)
+    # Print DNS Records found
+    for entry in full_ans:
+        if not entry.startswith(';'):
+            dns_found.append(entry)
 
-	dmarc_target = f'_dmarc.{domain}'
-	query = dnslib.DNSRecord.question(dmarc_target, 'TXT')
-	pkt = query.send('8.8.8.8', 53, tcp='UDP')
-	dmarc_ans = dnslib.DNSRecord.parse(pkt)
-	dmarc_ans = str(dmarc_ans)
-	dmarc_ans = dmarc_ans.split('\n')
-	dmarc_found = []
+    if not dns_found:
+        print(f'{R}[-] {C}DNS Records Not Found!{W}')
+        if output != 'None':
+            result.setdefault('dns', ['DNS Records Not Found'])
+    else:
+        for entry in dns_found:
+            print(f'{C}{entry}{W}')
+            if output != 'None':
+                result.setdefault('dns', []).append(entry)
 
-	for entry in dmarc_ans:
-		if entry.startswith('_dmarc'):
-			dmarc_found.append(entry)
+    dmarc_target = f'_dmarc.{domain}'
+    query = dnslib.DNSRecord.question(dmarc_target, 'TXT')
+    pkt = query.send('8.8.8.8', 53, tcp='UDP')
+    dmarc_ans = dnslib.DNSRecord.parse(pkt)
+    dmarc_ans = str(dmarc_ans)
+    dmarc_ans = dmarc_ans.split('\n')
+    dmarc_found = []
 
-	if not dmarc_found:
-		print(f'\n{R}[-] {C}DMARC Record Not Found!{W}')
-		if output != 'None':
-			result.setdefault('dmarc', ['DMARC Record Not Found!'])
-	else:
-		for entry in dmarc_found:
-			print(f'{C}{entry}{W}')
-			if output != 'None':
-				result.setdefault('dmarc', []).append(entry)
-	result.update({'exported': False})
+    # Query DMARC Record
+    for entry in dmarc_ans:
+        if entry.startswith('_dmarc'):
+            dmarc_found.append(entry)
 
-	if output != 'None':
-		data['module-DNS Enumeration'] = result
-		fname = f'{output["directory"]}/dns_records.{output["format"]}'
-		output['file'] = fname
-		export(output, data)
-	log_writer('[dns] Completed')
+    if not dmarc_found:
+        print(f'\n{R}[-] {C}DMARC Record Not Found!{W}')
+        if output != 'None':
+            result.setdefault('dmarc', ['DMARC Record Not Found!'])
+    else:
+        for entry in dmarc_found:
+            print(f'{C}{entry}{W}')
+            if output != 'None':
+                result.setdefault('dmarc', []).append(entry)
+    result.update({'exported': False})
+
+    # Export Results if specified
+    if output != 'None':
+        data['module-DNS Enumeration'] = result
+        fname = f'{output["directory"]}/dns_records.{output["format"]}'
+        output['file'] = fname
+        export(output, data)
+    log_writer('[dns] Completed')
